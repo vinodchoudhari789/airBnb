f84045c14d4ff9ecc4c793c1c141e1f0a73a30e1
Vinod Choudhari <vinodchoudhari722@gmail.com>
2026-07-19 12:00:00 +0530

    Base Version

---
+++ src/main/java/com/project/airBnbApp/AirBnbAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.airBnbApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBnbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirBnbAppApplication.class, args);
+	}
+
+}
