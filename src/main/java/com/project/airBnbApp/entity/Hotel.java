0635f5fcf4cd2ba16d5a91a44c9fdfd7e64aacba
Vinod Choudhari <vinodchoudhari722@gmail.com>
2026-07-20 17:26:23 +0530

    Creating Hotel Entity

---
+++ src/main/java/com/project/airBnbApp/entity/Hotel.java
@@ -0,0 +1,39 @@
+package com.project.airBnbApp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "hotel")
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String city;
+
+    @Column(columnDefinition = "TEXT[]")
+    private String[] photos;
+
+    @Column(columnDefinition = "TEXT[]")
+    private String[] amenities;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    private Boolean isActive;
+}
