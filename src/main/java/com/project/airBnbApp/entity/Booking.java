1cd7e7695bd244c689061a6d4f591a669b1cfee7
Vinod Choudhari <vinodchoudhari722@gmail.com>
2026-07-21 12:32:37 +0530

    Creating Many-to-Many Mapping Between Guest and Booking

--- src/main/java/com/project/airBnbApp/entity/Booking.java
+++ src/main/java/com/project/airBnbApp/entity/Booking.java
@@ -8,6 +8,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -46,6 +47,14 @@ public class Booking {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private BookingStatus bookingStatus;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "booking_guest",
+            joinColumns = @JoinColumn(name = "booking_id"),
+            inverseJoinColumns = @JoinColumn(name = "guest_id")
+    )
+    private Set<Guest> guestSet;
 
     @CreationTimestamp
     @Column(updatable = false)
