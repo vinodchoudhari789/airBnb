cac1203f826ed7945b8bcc34f2389d3b18cc6fac
Vinod Choudhari <vinodchoudhari722@gmail.com>
2026-08-07 13:03:10 +0530

    Creating WebSecurityConfig to add JWTAuthFilter in SecurityFilterChain

---
+++ src/main/java/com/project/airBnbApp/security/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.project.airBnbApp.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final JWTAuthFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.
+                csrf(csrfConfig -> csrfConfig.disable())
+                .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/admin/**").hasRole("HOTEL_MANAGER")
+                                .requestMatchers("/auth/**").anonymous()
+                                .requestMatchers("/bookings/**").authenticated()
+                                .anyRequest().permitAll()
+                        );
+        return httpSecurity.build();
+    }
+}
