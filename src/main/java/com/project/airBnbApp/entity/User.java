367d3644b6d90b7e410921987c083165c177053d
Vinod Choudhari <vinodchoudhari722@gmail.com>
2026-08-07 12:58:15 +0530

    Implementing UserDetails in User Entity

--- src/main/java/com/project/airBnbApp/entity/User.java
+++ src/main/java/com/project/airBnbApp/entity/User.java
@@ -4,14 +4,20 @@ import com.project.airBnbApp.entity.enums.Role;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "app_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,7 +26,7 @@ public class User {
     @Column(nullable = false)
     @ElementCollection(fetch = FetchType.EAGER)  // will create another table for roles
     @Enumerated(EnumType.STRING)
-    private Set<Role> Roles;
+    private Set<Role> roles;
 
     private String name;
 
@@ -29,4 +35,16 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_"+role.name()))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
