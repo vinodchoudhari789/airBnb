530307ffac933fb872f30a9eba0b3799bd72fef3
Vinod Choudhari <vinodchoudhari722@gmail.com>
2026-08-25 14:54:12 +0530

    "Minor Changes"

--- src/main/java/com/project/airBnbApp/service/HotelServiceImpl.java
+++ src/main/java/com/project/airBnbApp/service/HotelServiceImpl.java
@@ -19,7 +19,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.project.airBnbApp.util.AppUtils.getCurrentUser;
 
