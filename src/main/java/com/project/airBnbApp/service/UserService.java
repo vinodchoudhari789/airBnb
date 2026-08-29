15d509c311134e50429fe8fe90ada66e7dde55a5
Vinod Choudhari <vinodchoudhari722@gmail.com>
2026-08-29 12:47:09 +0530

    "Minor Changes"

--- src/main/java/com/project/airBnbApp/service/UserService.java
+++ src/main/java/com/project/airBnbApp/service/UserService.java
@@ -3,7 +3,6 @@ package com.project.airBnbApp.service;
 import com.project.airBnbApp.dto.ProfileUpdateRequestDTO;
 import com.project.airBnbApp.dto.UserDTO;
 import com.project.airBnbApp.entity.User;
-import org.jspecify.annotations.Nullable;
 
 public interface UserService{
 
