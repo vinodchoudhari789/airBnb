1cc57669850402dd947f9121f6cf4138422e27b5
Vinod Choudhari <vinodchoudhari722@gmail.com>
2026-09-06 10:55:08 +0530

    "Adding Health Check Controller"

---
+++ src/main/java/com/project/airBnbApp/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.project.airBnbApp.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> healthCheckController(){
+        return ResponseEntity.ok("OK");
+    }
+}
